d6e5064ec08b3bebd190ae9a845aed1c70e3be67
Ahasanul Hoque <ahoque24@gmail.com>
2020-08-03 15:00:09 -0400

    Now working for strung together operations

--- scripts/script.js
+++ scripts/script.js
@@ -29,29 +29,40 @@ operators.forEach((operator) => {
 
 equals.addEventListener('click', () => {
     let result = operate(calculator.operation, +calculator.firstNumber, +calculator.displayedNumber);
-    calculator.equalsChosen = true;
+    calculator.equalsChosen = true;   //Flip flag so the if statement in updateDisplay() is not carried out
     updateDisplay(result);
     calculator.equalsChosen = false;
-});
+    calculator.operation = '';       //Make this an empty string so operations can be carried out on the result
+});                                  //See the first condition in chooseOperation()
 
 clear.addEventListener('click', () => {
     calculator.displayedNumber = '';
     calculator.firstNumber = '';
     calculator.secondNumber = '';
     calculator.operation = '';
-    display.textContent = 0;
+    display.textContent = String.fromCharCode(160);
 });
 
 function updateDisplay(num){
-    if(!calculator.operationChosen && !calculator.equalsChosen) calculator.displayedNumber = calculator.displayedNumber + num;
+    //The if statement below allows the user to enter multiple digits
+    if(!calculator.operationChosen && !calculator.equalsChosen && calculator.displayedNumber != 'Err') calculator.displayedNumber = calculator.displayedNumber + num;
     else calculator.displayedNumber = num;
-    calculator.operationChosen = false;
+    calculator.operationChosen = false;             //Flip back to false to allow user enter multiple digits
     display.textContent = calculator.displayedNumber;
 }
 
 function chooseOperation(selectedOp){
+    //The if statement below allows the user to string together operations. The second part of the condition
+    //also checks to see if equals has just been pressed and an operation is being performed on the result.
+
+    //calculator.operation and calculator.opeartionChosen are used for separate checks: do I operate in background
+    //(used by this function, calculator.operation) or do I add multiple digits (used in updateDisplay(), calculator.opeartionChosen)
+    if(calculator.firstNumber != '' && calculator.operation != ''){
+        calculator.firstNumber = operate(calculator.operation, +calculator.firstNumber, +calculator.displayedNumber);
+    } else {
+        calculator.firstNumber = calculator.displayedNumber;
+    }
     calculator.operationChosen = true;
-    calculator.firstNumber = calculator.displayedNumber;
     if(selectedOp=="+") calculator.operation = "add";
     else if(selectedOp=="-") calculator.operation = "subtract";
     else if(selectedOp=="X") calculator.operation = "multiply";
@@ -60,6 +71,12 @@ function chooseOperation(selectedOp){
 
 //Takes in an operator, then calls the correct function
 function operate(operator, a, b){
+    if(operator == "divide" && b == 0){
+        alert("You cannot divide by 0!");
+        calculator.operationChosen = true;
+        return 'Err';
+    }
+
     if(operator == "add") return add(a,b);
     else if(operator == "subtract") return subtract(a,b);
     else if(operator == "multiply") return multiply(a,b);
