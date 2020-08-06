c3d0d3930ef061a8bafc3d42e30a381695ebffef
Ahasanul Hoque <ahoque24@gmail.com>
2020-08-06 12:35:17 -0400

    Fixed issue with decimal being entered multiple times on keyboard

--- scripts/script.js
+++ scripts/script.js
@@ -136,8 +136,7 @@ function deleteLastNumber(){
 //Keyboard support
 document.addEventListener('keydown', function(e) {
     if(e.key == '0' || e.key == '1' || e.key == '2' || e.key == '3' || e.key == '4' || 
-        e.key == '5' || e.key == '6' || e.key == '7' || e.key == '8' || e.key == '9' ||
-        e.key == '.'){
+        e.key == '5' || e.key == '6' || e.key == '7' || e.key == '8' || e.key == '9'){
         updateDisplay(e.key);
     } else if(e.key == '+' || e.key == '-' || e.key == 'X' || e.key == '/'){
         chooseOperation(e.key);
@@ -151,7 +150,8 @@ document.addEventListener('keydown', function(e) {
         showSolution();
     }
 
-    if(e.key == '.'){
+    if(e.key == '.' && !decimal.hasAttribute('disabled')){
+        updateDisplay(e.key);
         decimal.setAttribute('disabled', '');
     }
 });
